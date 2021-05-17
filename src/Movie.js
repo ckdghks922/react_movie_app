6ab046a4b78759a0c0d4bc07fae1f1b17ba8ecc9
ckdghks922 <ch.park@kaist.ac.kr>
2021-05-17 17:43:37 +0900

    chore: 화살표 함수로 변경

--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie({ id, year, title, summary, poster }) {
+const Movie = ({ id, year, title, summary, poster }) => {
   return <h4>{title}</h4>;
-}
+};
 
 Movie.propTypes = {
   id: PropTypes.number.isRequired,
